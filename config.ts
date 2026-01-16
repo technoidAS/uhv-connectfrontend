5931dc14053af893dba59d5c844b77291916a9a2
Ashutosh Singh <asks.it10@gmail.com>
2026-01-16 19:46:57 +0000

    vercel.json created

--- config.ts
+++ config.ts
@@ -1,4 +1,3 @@
-export const API_URL =
-  typeof window !== "undefined"
-    ? "https://uhv-connectbackend-zpkb.onrender.com/api"
-    : "https://uhv-connectbackend-zpkb.onrender.com/api";
+export const API_URL = 
+  process.env.EXPO_PUBLIC_API_URL || 
+  "https://uhv-connectbackend-zpkb.onrender.com/api";
